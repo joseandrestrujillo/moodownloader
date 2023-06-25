6372affbbed5816b3cfda0d94278778027ad3c96
José Andrés Trujillo <trujillohenaresj@gmail.com>
2023-06-25 19:41:36 +0200

    Login, list courses and coockies

--- moodownloader/app.py
+++ moodownloader/app.py
@@ -1,7 +1,41 @@
+from robobrowser import RoboBrowser
+from bs4 import BeautifulSoup
+import getpass
+import os.path
+
 def main():
     url = input("Introduzca la url del moodle que quieres descargar: ")
     url = "https://" + url
-    url_login = url + "login/index.php"
+
+    if os.path.isfile('cookies.txt'):
+        browser = RoboBrowser(parser='html.parser')
+        browser.session.cookies.load('cookies.txt', ignore_discard=True)
+    else:
+        url_login = url + "login/index.php"    
+
+        browser = RoboBrowser(parser='html.parser')
+        browser.open(url_login)
+
+        form = browser.get_form()
+        if form is not None:
+            username = input("Introduzca el nombre de usuario de moodle: ")
+            password = getpass.getpass('Introduzca la contrasena de moodle: ') 
+            form['username'].value = username
+            form['password'].value = password
+            browser.submit_form(form)
+            browser.session.cookies.save(ignore_discard=True)
+        else:
+            print('No se pudo encontrar el formulario de inicio de sesión.')
+            return
+        
+    browser.open(url)
+    soup = BeautifulSoup(browser.response.content, 'html.parser')
+    course_links = soup.find_all('div', {'class': 'coursebox'})
+
+    for link in course_links:
+        course_name = link.find('h3', {'class': 'coursename'})
+        if course_name is not None:
+            print(course_name.text)
 
 
 
