ddaf1b5d7533289a13459a8d6f7ab815a6f2039c
José Andrés Trujillo <trujillohenaresj@gmail.com>
2023-06-25 19:11:07 +0200

    Web scraping libraries and basic cli

---
+++ moodownloader/app.py
@@ -0,0 +1,9 @@
+def main():
+    url = input("Introduzca la url del moodle que quieres descargar: ")
+    url = "https://" + url
+    url_login = url + "login/index.php"
+
+
+
+if __name__ == '__main__':
+    main()
